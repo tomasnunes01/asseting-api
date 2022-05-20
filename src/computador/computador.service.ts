d735e5c132a33e54ebabfe2913c3eaf638066795
tomasnunes01 <tomasmnunes@gmail.com>
2022-05-20 12:00:16 +0100

    20 maio

--- src/computador/computador.service.ts
+++ src/computador/computador.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Inject, Body } from '@nestjs/common';
+import { ResultadoDto } from 'src/dto/resultado.dto';
 import { Repository, getRepository } from 'typeorm';
 import { Computador, TipoComputador } from './computador.entity';
+import { ComputadorRegisterDto } from './dto/computador.register.dto';
 
 @Injectable()
 export class ComputadorService {
@@ -13,5 +15,42 @@ export class ComputadorService {
 
   async listTypes(): Promise<TipoComputador[]> {
     return this.tipoComputadorRepository.find();
+  }
+
+  async registar(data: ComputadorRegisterDto): Promise<ResultadoDto> {
+    const computador = new Computador();
+    computador.nr_serie = data.nr_serie;
+    computador.cod_utilizador = data.username;
+    computador.cod_escritorio = data.cod_escritorio;
+    computador.cod_tipo = data.cod_tipo;
+    computador.marca = data.marca;
+    computador.modelo = data.modelo;
+    computador.descricao = data.descricao;
+    computador.so = data.so;
+    computador.cpu = data.cpu;
+    computador.ram = data.ram;
+    computador.hdd = data.hdd;
+    computador.garantia = data.garantia;
+    computador.data_instalacao = data.data_instalacao;
+    computador.fim_emprestimo = data.fim_emprestimo;
+    computador.aviso = data.aviso;
+    return this.computadorRepository
+      .save(computador)
+      .then(() => {
+        return <ResultadoDto>{
+          status: true,
+          mensagem: 'Computador adicionado!',
+        };
+      })
+      .catch((error) => {
+        return <ResultadoDto>{
+          status: false,
+          mensagem: 'Ocorreu um erro no pedido: ' + error,
+        };
+      });
+  }
+
+  async findAll(): Promise<Computador[]> {
+    return this.computadorRepository.find();
   }
 }
